1351e3ac11121b087e2c32112c64d5a9cdf3dd26
Maryucha_Mariani <https://Maryucha_Mariani@bitbucket.org/Maryucha_Mariani/appsensores.git>
2020-12-07 22:12:46 -0300

    Json e txt

--- BancoNovo/src/br/com/mariani/controle/ControleAgencia.java
+++ BancoNovo/src/br/com/mariani/controle/ControleAgencia.java
@@ -5,10 +5,17 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 import br.com.mariani.modelos.Cliente;
+import br.com.mariani.modelos.Conta;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 
 /**
- * 
- * Essa Classe tem o objetivo de encapsular os métodos referentes as ações da agencia bancária.
+ *
+ * Essa Classe tem o objetivo de encapsular os métodos referentes as ações da
+ * agencia bancária.
+ *
  * @author maryucha
  */
 public class ControleAgencia {
@@ -22,7 +29,7 @@ public class ControleAgencia {
      * Esse método roda o menu da agencia usando um switch
      *
      */
-    public void agencia() {
+    public void agencia() throws InterruptedException {
         do {
             try {
                 System.out.println("--------------AGENCIA BANCÁRIA------------"
@@ -32,7 +39,9 @@ public class ControleAgencia {
                         + "\n4 Mostrar Contas: "
                         + "\n5 Desvincular Cliente: "
                         + "\n6 Terminal Eletrônico: "
-                        + "\n7 Sair: "
+                        + "\n7 Jogar em um documento os cadastros: "
+                        + "\n8 Pegar de um documento: "
+                        + "\n9 Sair: "
                         + "\n------------------------------------------------------\n");
                 menu = entrada.nextInt();
                 entrada.nextLine();
@@ -61,6 +70,12 @@ public class ControleAgencia {
                         caixa.terminalEletronico();
                         break;
                     case 7:
+                        descarregarNoDocumento(listaClientes);
+                        break;
+                    case 8:
+                        cargaPorScanner();
+                        break;
+                    case 9:
                         System.out.println("Até mais!");
                         break;
                     default:
@@ -68,11 +83,12 @@ public class ControleAgencia {
                         break;
                 }
             }
-        } while (menu != 7);
+        } while (menu != 9);
     }
-/**
- * Esse método retorna todos os clientes e suas contas!
- */
+
+    /**
+     * Esse método retorna todos os clientes e suas contas!
+     */
     public void mostrarClientes() {
         System.out.println("------------Clientes CADASTRADOS----------");
         if (listaClientes.isEmpty()) {
@@ -89,11 +105,12 @@ public class ControleAgencia {
             }
         }
     }
-/**
- * Esse método percorre a lista de clientes através do busca cliente
- * e trás as contas cadastradas na lista de contas desse cliente.
- * 
- */
+
+    /**
+     * Esse método percorre a lista de clientes através do busca cliente e trás
+     * as contas cadastradas na lista de contas desse cliente.
+     *
+     */
     public void mostrarContaPorCliente() {
         System.out.println("------------CONTAS DO CLIENTE----------");
         Cliente cliente = bucarCliente();
@@ -105,10 +122,12 @@ public class ControleAgencia {
         } while (cliente.getListaContas().size() < 1);
 
     }
-/**
- * Esse método cadastra uma nova conta para o cliente
- * @obs: pretendo fazer uma validação da existência do tipo de conta
- */
+
+    /**
+     * Esse método cadastra uma nova conta para o cliente
+     *
+     * @obs: pretendo fazer uma validação da existência do tipo de conta
+     */
     public void cadNovaConta() {
         Cliente cliente = new Cliente();
         System.out.print("Digite o CPF do Cliente: ");
@@ -123,9 +142,10 @@ public class ControleAgencia {
             }
         }
     }
-/**
- * Esse método´percorre a lista de Clientes e remove ele pelo index;
- */
+
+    /**
+     * Esse método´percorre a lista de Clientes e remove ele pelo index;
+     */
     public void desvincularCliente() {
         System.out.print("Digite o CPF do Cliente: ");
         String nome = entrada.nextLine();
@@ -137,10 +157,12 @@ public class ControleAgencia {
             }
         }
     }
-/**
- * Esse método busca o meu cliente dentro do listaClientes dessa classe
- * @return cliente
- */
+
+    /**
+     * Esse método busca o meu cliente dentro do listaClientes dessa classe
+     *
+     * @return cliente
+     */
     public Cliente bucarCliente() {
         Cliente cliente;
         System.out.print("Digite o CPF do Cliente: ");
@@ -160,4 +182,72 @@ public class ControleAgencia {
         }
         return null;
     }
+
+    /**
+     * Jogar para um documento os dados
+     *
+     * @param listaClientes
+     */
+    public void descarregarNoDocumento(List<Cliente> listaClientes) {
+        String path = "D:\\Cursos\\SENAI\\VOID\\AulaInterfaces\\BancoNovo\\desev\\cadClientes.txt";
+
+        try (BufferedWriter bw = new BufferedWriter(new FileWriter(path))) {  // abre o arquivo
+            for (Cliente linhaCliente : listaClientes) {
+                bw.write("NOME: " + linhaCliente.getNome()
+                        + "\nCPF: " + linhaCliente.getCpf()
+                        + "\nCONTA: " + linhaCliente.getListaContas().iterator().next().getNumConta()
+                        + "\n------------------------------------------------------------------------------");
+                bw.newLine();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public List<Cliente> cargaPorScanner() throws InterruptedException {
+        File arquivo = new File("D:\\Cursos\\SENAI\\VOID\\AulaInterfaces\\BancoNovo\\desev\\cadClientes.txt");
+        Scanner sc = null;
+
+        try {
+            sc = new Scanner(arquivo);
+            while (sc.hasNextLine()) {
+
+                String linhaCliente = sc.nextLine();
+                String[] vec = linhaCliente.split(";");
+
+                System.out.println("Nome: " + vec[0]);
+                System.out.println("CPF: " + vec[1]);
+                System.out.println("NUMERO DA CONTA: " + vec[2]);
+                System.out.println("SALDO: " + vec[3]);
+                System.out.println("AGENCIA: " + vec[4]);
+                System.out.println("TIPO DA CONTA: " + vec[5]);
+
+                System.out.println();
+                Cliente e = new Cliente();
+                e.setNome(vec[0]);
+                e.setCpf(vec[1]);
+                for (int i = 0; i < e.getListaContas().size(); i++) {
+                    e.getListaContas().get(i).setNumConta(Integer.parseInt(vec[3]));
+                    e.getListaContas().get(i).setSaldo(Double.parseDouble(vec[4]));
+                    e.getListaContas().get(i).setAgencia(Integer.parseInt(vec[5]));
+                    e.getListaContas().get(i).setTipoConta(vec[6]);
+                }
+
+                listaClientes.add(e);
+
+            }
+        } catch (IOException e) {
+            System.out.println("Erro: " + e.getMessage());
+        } finally {
+            if (sc != null) {
+                sc.close();
+
+                System.out.println("Carregando...");
+                Thread.sleep(1000);
+                System.out.println("Produtos carregados com sucesso!");
+            }
+        }
+        return listaClientes;
+    }
+
 }
